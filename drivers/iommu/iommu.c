07e5e71f46b55a377d74088492f7a55fef25d1ec
Stepan Moskovchenko <stepanm@codeaurora.org>
2011-08-11 19:32:27 -0700

    drivers: iommu: Add map/unmap range ops
    commit 0b1345ebd586363a51c7a380fb3e7b3f6d81b445 from
    git://codeaurora.org/quic/le/kernel/msm msm-3.4
    
    Add IOMMU ops functions to allow mapping and unmapping
    whole ranges of address space based on a scatterlist.
    
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -336,6 +336,30 @@ size_t iommu_unmap(struct iommu_domain *domain, unsigned long iova, size_t size)
 }
 EXPORT_SYMBOL_GPL(iommu_unmap);
 
+int iommu_map_range(struct iommu_domain *domain, unsigned int iova,
+		    struct scatterlist *sg, unsigned int len, int prot)
+{
+	if (unlikely(domain->ops->map_range == NULL))
+		return -ENODEV;
+
+	BUG_ON(iova & (~PAGE_MASK));
+
+	return domain->ops->map_range(domain, iova, sg, len, prot);
+}
+EXPORT_SYMBOL_GPL(iommu_map_range);
+
+int iommu_unmap_range(struct iommu_domain *domain, unsigned int iova,
+		      unsigned int len)
+{
+	if (unlikely(domain->ops->unmap_range == NULL))
+		return -ENODEV;
+
+	BUG_ON(iova & (~PAGE_MASK));
+
+	return domain->ops->unmap_range(domain, iova, len);
+}
+EXPORT_SYMBOL_GPL(iommu_unmap_range);
+
 int iommu_device_group(struct device *dev, unsigned int *groupid)
 {
 	if (iommu_present(dev->bus) && dev->bus->iommu_ops->device_group)
