73b296f0ab4d5c5e1ee346e9569976d844977649
Zoran Markovic <zmarkovic@sierrawireless.com>
2014-09-19 13:52:14 -0700

    TRAC-1547: disable deferred resume of HCD bus
    HSIC driver configured the HCD bus for deferred resume. This left
    the HCD bus in a "resuming" state forever, preventing subsequent
    system suspend requests, as described in TRAC 1547 and (possibly)
    1556. This is also the root cause of ethernet crash and suspend
    failures described in TRAC 1227.
    
    Clearing the "skip_resume" flag causes the HCD bus to resume in the
    foreground, thus rendering all connected devices operational.
    
    Signed-off-by: Zoran Markovic <zmarkovic@sierrawireless.com>
    Signed-off-by: Dragan Marinkovic <dmarinkovi@sierrawireless.com>

--- drivers/usb/host/ehci-msm-hsic.c
+++ drivers/usb/host/ehci-msm-hsic.c
@@ -1598,7 +1598,7 @@ static int __devinit ehci_hsic_msm_probe(struct platform_device *pdev)
 		return  -ENOMEM;
 	}
 
-	hcd_to_bus(hcd)->skip_resume = true;
+	hcd_to_bus(hcd)->skip_resume = false;
 
 	hcd->irq = platform_get_irq(pdev, 0);
 	if (hcd->irq < 0) {
